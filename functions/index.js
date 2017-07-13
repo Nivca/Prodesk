6a7a94df561d25d00daf988b36513d1cc9b2d0f5
Anenth <anenthvishnu@gmail.com>
2017-07-13 22:02:07 +0530

    Remova cloud function for build to pass

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,41 @@
 
-var functions = require('firebase-functions');
-const TOKEN = functions.config().sendgrid.token;
-var sendGrid = require('sendgrid')(TOKEN);
+// var functions = require('firebase-functions');
+// const TOKEN = functions.config().sendgrid.token;
+// var sendGrid = require('sendgrid')(TOKEN);
 
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+// // // Create and Deploy Your First Cloud Functions
+// // // https://firebase.google.com/docs/functions/write-firebase-functions
+// //
+// // exports.helloWorld = functions.https.onRequest((request, response) => {
+// //  response.send("Hello from Firebase!");
+// // });
+
+
+// function parseBody() {
+//     var helper = require('sendgrid').mail;
+//     const FROM_EMAIL = "hello@prodesk.life";
+    
+//     var fromEmail = new helper.Email(FROM_EMAIL);
+//     var toEmail = new helper.Email('test@example.com');
+//     var subject = 'Thank you';
+//     var content = new helper.Content('text/html', 'Thank you for your interest');
+
+//     var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+//     return mail.toJSON();
+// }
+
+// var request = sendGrid.emptyRequest({
+//   method: 'POST',
+//   path: '/v3/mail/send',
+//   body: parseBody()
 // });
 
-
-function parseBody() {
-    var helper = require('sendgrid').mail;
-    const FROM_EMAIL = "hello@prodesk.life";
-    
-    var fromEmail = new helper.Email(FROM_EMAIL);
-    var toEmail = new helper.Email('test@example.com');
-    var subject = 'Thank you';
-    var content = new helper.Content('text/html', 'Thank you for your interest');
-
-    var mail = new helper.Mail(fromEmail, subject, toEmail, content);
-    return mail.toJSON();
-}
-
-var request = sendGrid.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: parseBody()
-});
-
-sendGrid.API(request, function (error, response) {
-  if (error) {
-    console.log('Error response received');
-  }
-  console.log(response.statusCode);
-  console.log(response.body);
-  console.log(response.headers);
-});
+// sendGrid.API(request, function (error, response) {
+//   if (error) {
+//     console.log('Error response received');
+//   }
+//   console.log(response.statusCode);
+//   console.log(response.body);
+//   console.log(response.headers);
+// });
