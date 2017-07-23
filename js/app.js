96f72c6b7287e8ec7b8d116faaf16a4aee607563
Anenth <anenthvishnu@gmail.com>
2017-07-23 20:39:14 +0530

    Refactor: move js to app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
-(function () {
+function lazyLoadImages() {
+    var imgElement = document.querySelectorAll('.lazy');
+    for (var i = 0; i < imgElement.length; ++i) {
+        imgElement[i].onload = function () {
+            this.className += " loaded";
+        }
+        if (imgElement[i].complete) {
+            imgElement[i].className += " loaded";
+        }
+    }
+}
+
+function writeUserData(data) {
     var config = {
         apiKey: "AIzaSyAUB0d_CiOAspF1kuaajU8OhfM2DYqo3HQ",
         authDomain: "healthydesk-75f21.firebaseapp.com",
@@ -10,22 +22,22 @@
     firebase.initializeApp(config);
 
     var database = firebase.database();
+    
+    firebase.database().ref('request/').push({
+        name: data.name,
+        email: data.email,
+        phone: data.phone,
+        no_units: data.no_units || 0,
+        message: data.message
+    }).then(function() {
+        $('.js-place-order')[0].reset();
+        $('.js-success').slideDown();
+    }).catch(function() {
+        $('.js-error').slideDown();
+    });
+}
 
-    function writeUserData(data) {
-        firebase.database().ref('request/').push({
-            name: data.name,
-            email: data.email,
-            phone: data.phone,
-            no_units: data.no_units || 0,
-            message: data.message
-        }).then(function() {
-            $('.js-place-order')[0].reset();
-            $('.js-success').slideDown();
-        }).catch(function() {
-            $('.js-error').slideDown();
-        });
-    }
-
+function handleFormSubmission() {
     $('.js-place-order').on('submit', function(e) {
         e.preventDefault();
         $('.js-success, .js-error').slideUp();
@@ -38,4 +50,32 @@
        
         writeUserData({name: name, email: email, phone: phone, no_units: no_units, message: message});
     });
+}
+
+function initImageSlider() {
+     var swiper = new Swiper('.js-swiper-container', {
+        nextButton: '.js-swiper-button-next',
+        prevButton: '.js-swiper-button-prev',
+        slidesPerView: 3,
+        spaceBetween: 30,
+        zoom: true,
+        freeMode: true,
+        preloadImages: false,
+        lazyLoading: true,
+        breakpoints: {
+            640: {
+                slidesPerView: 1
+            }
+        }
+    });
+}
+ 
+function init() {
+    lazyLoadImages();
+    handleFormSubmission();
+    initImageSlider();
+}
+
+(function(){
+    init();
 })();
